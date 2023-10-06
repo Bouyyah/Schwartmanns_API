4bd4bd1a93d0bc2b6c723927856a90c254f67f24
Ayyoub Hamame <Hamameayyoub@gmail.com>
2023-10-06 11:31:32 +0100

    Add project files.

---
+++ Schwartmanns/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace Schwartmanns.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
